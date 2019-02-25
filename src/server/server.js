8ff41636ef1f373d06cd5511c3d09bb3ad2ae004
kacper.sledz <casper.sledx@gmail.com>
2019-02-25 21:21:22 +0100

    test3

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,29 @@
-var app = require('express')();
+const express = require('express');
+const path = require('path');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../../build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req, res) => {
+  var list = ['item1', 'item2', 'item3'];
+  res.json(list);
+  console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../../build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+http.listen(PORT, function() {
+  console.log('listening on *:' + PORT);
+});
 
 io.on('connection', function(socket) {
   socket.on('ROOM_CONNECT', (...args) => {
@@ -53,10 +76,4 @@ io.on('connection', function(socket) {
     console.log(msg);
     socket.broadcast.to(room).emit('CELL_CLICK', msg);
   });
-});
-
-const PORT = process.env.PORT || 5000;
-
-http.listen(PORT, function() {
-  console.log('listening on *:' + PORT);
 });
