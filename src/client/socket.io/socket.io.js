06dc37ff50234f42f31b4305bfe6586a0a3c3a33
kacper.sledz <casper.sledx@gmail.com>
2019-02-25 21:28:17 +0100

    test4

--- src/client/socket.io/socket.io.js
+++ src/client/socket.io/socket.io.js
@@ -3,6 +3,12 @@ import io from 'socket.io-client';
 const PORT = process.env.PORT || 5000;
 const socket = io('https://tic-tac-toe-oxo.herokuapp.com:' + PORT);
 
+var sockett = io.connect();
+console.log('check 1', sockett.connected);
+sockett.on('connect', function() {
+  console.log('check 2', sockett.connected);
+});
+
 socket.on('CELL_CLICK', msg => {
   store.dispatch({ type: 'CELL_CLICK', id: msg });
 });
