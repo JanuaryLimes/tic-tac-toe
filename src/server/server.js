ea3f80f0b9b12534f5d30c07fdf6a4e695f682a4
kacper.sledz <casper.sledx@gmail.com>
2019-02-27 17:55:21 +0100

    force https 2

--- src/server/server.js
+++ src/server/server.js
@@ -4,20 +4,19 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(function(environments, status) {
-  environments = environments || ['production'];
-  status = status || 302;
-  return function(req, res, next) {
-    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(status, 'https://' + req.hostname + req.originalUrl);
-      } else {
-        next();
-      }
+app.use((req, res, next) => {
+  const environments = ['production'];
+  const status = 302;
+
+  if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+      res.redirect(status, 'https://' + req.hostname + req.originalUrl);
     } else {
       next();
     }
-  };
+  } else {
+    next();
+  }
 });
 
 // Serve the static files from the React app
