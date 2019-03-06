4bf290b305f47169f7d69a486e3e353e4e4d05fa
kacper.sledz <casper.sledx@gmail.com>
2019-03-06 19:29:08 +0100

    Style for icon info in footer

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -7,6 +7,7 @@ import { emitToRoom } from '../socket.io/socket.io';
 import PropTypes from 'prop-types';
 import ConnectionStatus from './ConnectionStatus';
 import { store } from '../redux/store';
+import Footer from './Footer';
 
 class Main extends Component {
   componentDidMount() {
@@ -80,35 +81,7 @@ class Main extends Component {
           )}
         </div>
         <TicTacToe />
-        <div className="footer">
-          <div>
-            Icons made by{' '}
-            <a
-              href="https://www.flaticon.com/authors/roundicons"
-              title="Roundicons"
-              rel="noopener noreferrer"
-            >
-              Roundicons
-            </a>{' '}
-            from{' '}
-            <a
-              href="https://www.flaticon.com/"
-              rel="noopener noreferrer"
-              title="Flaticon"
-            >
-              www.flaticon.com
-            </a>{' '}
-            is licensed by{' '}
-            <a
-              href="http://creativecommons.org/licenses/by/3.0/"
-              title="Creative Commons BY 3.0"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CC 3.0 BY
-            </a>
-          </div>
-        </div>
+        <Footer />
       </div>
     );
   }
