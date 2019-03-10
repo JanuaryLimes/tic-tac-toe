d19a258f9449fbfa8cc82bdfe8b0e03e39963b8a
kacper.sledz <casper.sledx@gmail.com>
2019-03-10 10:29:15 +0100

    ... #22

--- src/client/socket.io/socket.io.js
+++ src/client/socket.io/socket.io.js
@@ -23,21 +23,9 @@ socket.on('CELL_CLICK', msg => {
   const thunkRestartGame = () => (dispatch, getState) => {
     const { gameOver } = getState().tictactoe;
     if (gameOver) {
-      console.log('GAME_OVER_START');
-      dispatch({ type: 'GAME_OVER_START' });
       setTimeout(() => {
-        console.log('emit to room nowa gra');
         emitToRoom('NEW_GAME');
       }, 3000);
-
-      //todo: wszystkie emity trzeba chyba przerobic na io.in(room)...
-
-      // show short animation, and update results
-      // wait
-      // emit restart
-
-      //dispatch() cos...
-      console.log('gra skończona, state: ', getState());
     }
   };
 
